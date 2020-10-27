bc029b1b9dd80936e1407b5e831a83fb4108ae03
RajibMiah <51170219+RajibMiah@users.noreply.github.com>
2020-10-27 09:05:03 -0700

    Add files via upload

---
+++ controls/controls.js
@@ -0,0 +1,16 @@
+import {Input} from './Input'
+import {RadioGroupContent }from './RadioGroup'
+import select from './select'
+import CheckBox from './CheckBox'
+import DatePiker from './DatePiker'
+import Button from './Button'
+import ActionButton from './ActionButton'
+export const  Controls  = {
+  Input,
+  RadioGroupContent,
+  select,
+  CheckBox,
+  DatePiker,
+  Button,
+  ActionButton,
+}
